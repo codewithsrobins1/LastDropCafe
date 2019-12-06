5018ecbf0b003b32da7fddeec42f58cfff41065c
codewithsrobins1 <codewithsrobins1@gmail.com>
2019-12-05 23:31:03 -0800

    120519

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Home Showcase Slider every 5 seconds
+//Write a Function that changes the photo of the slideshow;
+var slider = document.getElementById('showCaseSlider');
+var slideImages = ["img/showcase1.jpg", "img/showcase2.jpg", "img/showcase3.jpg"];
+var currentImage = 0;
+
+slider.
+
+function changeImg(){
+
+}
+
+
+
+
+
+//Form Validation For Contact Form
